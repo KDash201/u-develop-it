ad11d3399a04fe4d0fde0a65c48490391475b082
KDash201 <KVonMadison@Yahoo.com>
2022-01-19 20:32:17 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -91,9 +91,9 @@ app.post("/api/candidate", ({ body }, res) => {
   }
 
   // Create a candidate
-  const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-              VALUES (?,?,?,?)`;
-  const params = [1, "Ronald", "Firbank", 1];
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
     if (err) {
